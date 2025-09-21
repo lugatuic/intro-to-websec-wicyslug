588929b27b97ee72317672bf7db3c31118b03eb8
xmasscan <mkhan398@uic.edu>
2025-09-21 16:09:18 -0500

    Fixed embarrassing bug and culled DB entries
    I spent hours trying to understand why none of my SQL requests were
    going through. It was because I was using the username field for both
    the username and password. Woops!
    
    Also, users.db had 100 entries. 50 is already more than enough, so I
    deleted all non-admin entries and did just that :)

--- server/app/server.py
+++ server/app/server.py
@@ -3,6 +3,7 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -10,14 +11,21 @@ def home():
 @app.route('/login', methods=["POST"])
 def login():
     username = request.form.get("username")
-    password = request.form.get("username")
+    password = request.form.get("password")
 
+    # Connect to database
+    dbConn = sqlite3.connect("app/users.db")
+    cursor = dbConn.cursor()
     # SQL Injection Vulnerability
-    # TODO: SQLite connection
+    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+    result = cursor.execute(query).fetchone()
 
-    response = make_response(redirect("/dashboard"))
-    response.set_cookie("session", username) # Insecure session management
-    return response
+    if result:
+        response = make_response(redirect("/dashboard"))
+        response.set_cookie("session", username)
+        return response
+    else:
+        return f"Invalid credentials. <a href='/'>Try again</a><br/><p>{query}</p>"
 
 @app.route("/dashboard")
 def dashboard():
